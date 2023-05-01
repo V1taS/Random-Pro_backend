e7fe82fcd878123d20237bf5b51547389b68c1e1
V1taS <SosinVitalii@iCloud.com>
2023-05-01 19:18:08 +0200

    Initial

---
+++ src/test/kotlin/com/sosinvitalii/ApplicationTest.kt
@@ -0,0 +1,21 @@
+package com.sosinvitalii
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+import io.ktor.http.*
+import com.sosinvitalii.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
